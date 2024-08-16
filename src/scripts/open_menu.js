7962b7ed51b8b3b0db143c02dcd6998fb5f4a22e
Victor-Cantun <90561168+Victor-Cantun@users.noreply.github.com>
2024-08-16 09:09:03 -0600

    config movil

--- src/scripts/open_menu.js
+++ src/scripts/open_menu.js
@@ -13,3 +13,21 @@ document.querySelector("#btn_gaceta").addEventListener("click", () => {
 document.querySelector("#btn_ayuntamiento").addEventListener("click", () => {
   document.querySelector("#menu_ayuntamiento").classList.toggle("hidden");
 });
+/* MOVIL*/
+document.querySelector("#btn-movil_inicio").addEventListener("click", () => {
+  document.querySelector("#movil_menu").classList.add("hidden");
+  document.querySelector("#menu_ayuntamiento").classList.add("hidden");
+});
+document.querySelector("#btn-movil_ayuntamiento").addEventListener("click", () => {
+  document.querySelector("#movil_menu").classList.add("hidden");
+  document.querySelector("#menu_ayuntamiento").classList.toggle("hidden");
+});
+document.querySelector("#btn-movil_dependencias").addEventListener("click", () => {
+  document.querySelector("#movil_menu").classList.add("hidden");
+});
+document.querySelector("#btn-movil_transparencia").addEventListener("click", () => {
+  document.querySelector("#menu-movil_transparencia").classList.toggle("hidden");
+});
+document.querySelector("#btn-movil_gaceta").addEventListener("click", () => {
+  document.querySelector("#menu-movil_gaceta").classList.toggle("hidden");
+});
