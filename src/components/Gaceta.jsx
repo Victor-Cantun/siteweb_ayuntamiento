7266c0caf211d5e9870a25f3ac07ca23f2e6d332
Victor Cantun <victorcantun@icloud.com>
2025-04-22 06:59:22 -0600

    update style gaceta

--- src/components/Gaceta.jsx
+++ src/components/Gaceta.jsx
@@ -36,7 +36,7 @@ const [years, setYears] = useState([]);
 
     return (
         <>
-            <h1 class="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white uppercase">Gaceta municipal</h1>
+            <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white uppercase">Gaceta municipal</h1>
             <Accordion collapseAll  className="w-full">
             {years.map((item,index)=>(
             <Accordion.Panel key={index}>
